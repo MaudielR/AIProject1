d9f55be7a3d46da59189f32d93fde98d11134d0b
Maudiel Romero <41795758+MaudielR@users.noreply.github.com>
2020-10-10 23:28:00 -0400

    testing matrix

--- AIproject1.py
+++ AIproject1.py
@@ -1,4 +1,30 @@
-rows, cols = (7,5)
-arr = [[0]*cols]*rows
+import random
 
-print(arr) 
+
+rows, cols = (120,160)
+arr = [[1]*cols]*rows
+
+matrix = [[0 for i in range(cols)] for j in range(rows)]
+
+
+count = 31
+regions = 8
+while regions != 0:
+  
+  RandomRows = random.randint(0,119)
+  RandomCols = random.randint(0,159)
+  A = matrix[RandomRows][RandomCols]
+  B = matrix[RandomRows -31][RandomCols -31]
+  matrix[RandomRows][RandomCols] = 1
+  for isinstances in range(A,B):
+    matrix[RandomRows][RandomCols-1] =1
+    RandomCols = RandomCols -1
+    for areas in range(A,B): 
+      matrix[RandomRows-1][RandomCols] = 1
+      RandomRows = RandomRows - 1
+  
+    
+
+  regions = regions-1
+
+print('\n'.join(['\t'.join([str(cell) for cell in row]) for row in matrix]))
--- grid.py
+++ grid.py
@@ -1,6 +1,67 @@
+import random
+
 rows, cols = (120,160)
 arr = [[1]*cols]*rows
 
-for instances in arr: 
-  print(instances)
+matrix = [[0 for i in range(cols)] for j in range(rows)]
+
+
+
+regions = 100
+while regions != 0:
+  count = 5
+  RandomRows = random.randint(0,119)
+  RandomCols = random.randint(0,159) 
+  
+  
+  matrix[RandomRows][RandomCols] = 1
+  matrix[RandomRows-1][RandomCols] = 1
+  matrix[RandomRows-2][RandomCols] = 1
+  matrix[RandomRows-3][RandomCols] = 1
+  matrix[RandomRows-4][RandomCols] = 1
+  matrix[RandomRows-5][RandomCols] = 1
+  matrix[RandomRows][RandomCols-1] = 1
+  matrix[RandomRows][RandomCols-2] = 1
+  matrix[RandomRows][RandomCols-3] = 1
+  matrix[RandomRows][RandomCols-4] = 1
+  matrix[RandomRows][RandomCols -5] = 1
+  matrix[RandomRows -1][RandomCols -1] = 1
+  matrix[RandomRows -1][RandomCols -2] = 1
+  matrix[RandomRows -1][RandomCols -3] = 1
+  matrix[RandomRows -1][RandomCols -4] = 1
+  matrix[RandomRows -1][RandomCols -5] = 1
+
+  matrix[RandomRows -2][RandomCols -1] = 1
+  matrix[RandomRows -3][RandomCols -1] = 1
+  matrix[RandomRows -4][RandomCols -1] = 1
+  matrix[RandomRows -5][RandomCols -1] = 1
+
+  matrix[RandomRows -2][RandomCols -2] = 1
+  matrix[RandomRows -2][RandomCols -3] = 1
+  matrix[RandomRows -2][RandomCols -4] = 1
+  matrix[RandomRows -2][RandomCols -5] = 1
+
+  matrix[RandomRows -3][RandomCols -2] = 1
+  matrix[RandomRows -4][RandomCols -2] = 1
+  matrix[RandomRows -5][RandomCols -2] = 1
+
+  matrix[RandomRows -3][RandomCols -3] = 1
+  matrix[RandomRows -3][RandomCols -4] = 1
+  matrix[RandomRows -3][RandomCols -5] = 1
+  matrix[RandomRows -5][RandomCols -3] = 1
+  matrix[RandomRows -4][RandomCols -3] = 1
+  matrix[RandomRows -4][RandomCols -4] = 1
+  matrix[RandomRows -4][RandomCols -5] = 1
+
+  matrix[RandomRows -5][RandomCols -4] = 1
+
+ 
+  matrix[RandomRows -5][RandomCols -5] = 1
+  
+  regions = regions-1
+
+print('\n'.join(['\t'.join([str(cell) for cell in row]) for row in matrix]))
+  
+
+  
  
