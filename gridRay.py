919f48e0e62332aeb0bf89ee60e62ff68996007b
RayRCaringal <rayrcaringal@gmail.com>
2020-10-12 19:14:38 -0400

    Syntax changes

--- gridRay.py
+++ gridRay.py
@@ -11,7 +11,7 @@ import random
 
 # Use to build highway
 def build_highway(prevMatrix, dir):
-    if x is 119 or y is 159:
+    if x == 119 or y == 159:
         return "F"
     curMatrix = prevMatrix
     rC = random.randint(0, 100)
@@ -23,25 +23,25 @@ def build_highway(prevMatrix, dir):
             prevMatrix[:] = curMatrix
             return "R"
     else:
-        if dir is "N":
+        if dir == "N":
             if move(prevMatrix, "E"):
                 return "E"
             else:
                 prevMatrix[:] = curMatrix
                 return "R"
-        elif dir is "S":
+        elif dir == "S":
             if move(prevMatrix, "W"):
                 return "W"
             else:
                 prevMatrix[:] = curMatrix
                 return "R"
-        elif dir is "E":
+        elif dir == "E":
             if move(prevMatrix, "S"):
                 return "S"
             else:
                 prevMatrix[:] = curMatrix
                 return "R"
-        elif dir is "W":
+        elif dir == "W":
             if move(prevMatrix, "N"):
                 return "N"
             else:
@@ -54,52 +54,53 @@ def move(matrix, dir):
     global y
     global count
     mSize = 21
-    if dir is "N":
-        if mSize - 1 + y >= 159:
+    if dir =="N":
+        if mSize - 1 + y > 159:
             mSize = 159 - y
         for i in range(1, mSize):
-            if matrix[x][y + i] is "a" or "b":
+            if matrix[x][y + i] == "a" or  matrix[x][y + i] == "b":
                 return False
-            if matrix[x][y + i] is "1":
+            if matrix[x][y + i] == "1":
                 matrix[x][y + i] = "a"
             else:
                 matrix[x][y + i] = "b"
         y = y + mSize - 1
-    elif dir is "S":
-        if y - mSize - 1 <= 0:
+    elif dir == "S":
+        if y - mSize - 1 < 0:
             mSize = y + 1
         for i in range(1, mSize):
-            if matrix[x][y - i] is "a" or "b":
+            if matrix[x][y - i] == "a" or matrix[x][y + i] == "b":
                 return False
-            if matrix[x][y - i] is "1":
+            if matrix[x][y - i] == "1":
                 matrix[x][y - i] = "a"
             else:
                 matrix[x][y - i] = "b"
         y = y - mSize - 1
-    elif dir is "E":
-        if mSize - 1 + x >= 119:
+    elif dir == "E":
+        if mSize - 1 + x > 119:
             mSize = 119 - x
         for i in range(1, mSize):
-            if matrix[x + i][y] is "a" or "b":
+            if matrix[x + i][y] == "a" or matrix[x][y + i] == "b":
                 return False
-            if matrix[x + i][y] is "1":
+            if matrix[x + i][y] == "1":
                 matrix[x + i][y] = "a"
             else:
                 matrix[x + i][y] = "b"
         x = x + mSize - 1
-    elif dir is "W":
-        if x - mSize - 1 <= 0:
+    elif dir == "W":
+        if x - mSize - 1 < 0:
             mSize = x + 1
         for i in range(1, mSize):
-            if matrix[x - i][y] is "a" or "b":
+            if matrix[x - i][y] == "a" or matrix[x][y + i] == "b":
                 return False
-            if matrix[x - i][y] is "1":
+            if matrix[x - i][y] == "1":
                 matrix[x - i][y] = "a"
             else:
                 matrix[x - i][y] = "b"
         x = x + mSize - 1
     count += mSize
     return True
+
 
 """
 Harder to Traverse Cells 
@@ -116,15 +117,15 @@ matrix = [["1" for i in range(cols)] for j in range(rows)]
 
 regions = 0
 while regions < 8:
-    hX = random.randint(0,119)
-    hY = random.randint(0,159)
+    hX = random.randint(0, 119)
+    hY = random.randint(0, 159)
     if hX - 16 >= 0 and hX + 15 <= 119 and hY - 16 >= 0 and hY + 15 <= 159:
-        for i in range(hX-16, hX+16):
-            for j in range(hY-16, hX+15):
-                r = random.randint(0,2)
-                if r is 0:
+        for i in range(hX - 16, hX + 16):
+            for j in range(hY - 16, hX + 15):
+                r = random.randint(0, 2)
+                if r == 0:
                     matrix[i][j] = "2"
-            regions+=1
+            regions += 1
 
 """
 Select 4 Paths
@@ -137,7 +138,6 @@ Select 4 Paths
 -If length of highway is less than 100 then reject the path and restart
 -if you cannot add a highway given the placement of the previous rivers, FULL RESTART
 """
-
 
 # A lot of what is done here is purely for randomization
 paths = 0
@@ -155,10 +155,10 @@ while paths < 4:
         y = 0
         direction = "N"
     attempts = 0
-    while direction is not "F":
+    while direction != "F":
         oldDir = direction
         direction = build_highway(matrix, direction)
-        if direction is "R":
+        if direction == "R":
             attempts += 1
             direction = oldDir
         else:
@@ -172,8 +172,6 @@ while paths < 4:
                 break
     if count > 100:
         paths += 1
-
-
 
 """
 (After Select Paths)
